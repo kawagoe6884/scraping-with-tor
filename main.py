cd36d0c07ca4d30c2a6728e9b4b9bfe62c41ad24
kawagoe <97506026+kawagoe6884@users.noreply.github.com>
2022-05-02 11:26:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-# ------ Tor の起動 ------
+# ------ Starting Tor ------
 def Start_tor():
     global running
-    # kill
+    ## kill
     subprocess.call(r'taskkill /F /T /IM tor.exe', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     time.sleep(1)
-    # run
+    ## run
     Tor = f'C:\\Users\\{getpass.getuser()}\\Desktop\\Tor Browser\\Browser\\TorBrowser\\Tor\\tor.exe'
     running = subprocess.Popen(Tor)
     time.sleep(1)
@@ -26,11 +26,17 @@ def Start_tor():
 
 # ------ Starting selenium ------
 def Start_selenium():
-    # kill
+    ## kill
     subprocess.call(r'taskkill /F /T /IM chromedriver.exe', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     time.sleep(1)
-    # ------ ChromeDriver option ------
+    ## ------ ChromeDriver option ------
     options = Options()
+    if 'running' in globals():
+        ### Tor Proxy Settings
+        options.add_argument('--proxy-server=socks5://localhost:9050')
+        ### Define User-Agent
+        UA = UserAgent().chrome
+        options.add_argument('--user-agent=' + UA)
     options.add_argument('--blink-settings=imagesEnabled=false')                    # 画像の非表示
     options.add_argument('--disable-blink-features=AutomationControlled')           # navigator.webdriver=false とする設定
     options.add_argument('--disable-browser-side-navigation')                       # Timed out receiving message from renderer: の修正
@@ -53,27 +59,27 @@ def Start_selenium():
     options.add_experimental_option('prefs', prefs)
     options.add_experimental_option('useAutomationExtension', False)                # 拡張機能の自動更新を停止
 
-    # ------ Launch of ChromeDriver ------
+    ## ------ Launch of ChromeDriver ------
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     return driver
 
 
-# ------ driver.getのループ ------
+# ------ driver.get LOOP ------
 def Selenium(url):
     global driver
-    # Webアクセスのリトライ回数を指定する
+    ## Webアクセスのリトライ回数を指定する
     retry_count = 5
     try:
         for count in range(retry_count + 1):
             count += 1
-            # リトライ回数の上限を超えた場合はエラーにする
+            ### リトライ回数の上限を超えた場合はエラーにする
             if count > retry_count:
                 raise Exception('リトライ回数の上限を超えました')
-            # driver.get ⇒ 待機、エラーならtorとselenium起動
+            ### driver.get ⇒ 待機、エラーならtorとselenium起動
             try:
                 driver.get(url)
             except:
-                # Start_tor() # Tor 使わないならコメントアウト
+                Start_tor() # Tor を使わないならコメントアウト
                 driver = Start_selenium()
             else:
                 WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located)
